377585c3b90f0eb4213f55cd82960e677950dc03
DeroPlayz <88916740+DeroPlayz@users.noreply.github.com>
2025-04-04 17:34:12 +0000

    omg it worked!

---
+++ vpk/build/CMakeFiles/pspMouse.velf.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pspMouse.velf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pspMouse.velf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ vpk/build/CMakeFiles/vpksend.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vpksend"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/vpksend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
